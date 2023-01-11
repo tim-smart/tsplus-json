010f11cedbe202fea5c416d273b65ad1735b68f5
Tim Smart <tim.smart@arisechurch.com>
2023-01-12 10:17:25 +1300

    chore: cp README

--- generate.sh
+++ generate.sh
@@ -58,6 +58,7 @@ for project in config/*; do
 
   cd ../dist
   package_json "@tsplus-json/${project_name}" "$dist_version" "$package_name" "$latest_version" > package.json
+  cp "$cwd/README.md" .
 
   npm publish || true
 
